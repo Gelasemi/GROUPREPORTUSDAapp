6dfa3807cf88098c4145dab9632e6f9311b2c6bc
MANANTENASOA DAUPHIN GELASE MICHELOT <gelasemi@gmail.com>
2025-09-03 15:44:30 +0300

    Update GROUPREPORTapp.py

--- GROUPREPORTapp.py
+++ GROUPREPORTapp.py
@@ -78,11 +78,11 @@ if 'data' in st.session_state and st.session_state.data:
     if 'selected_sheet' in locals() and selected_sheet:
         st.header(f"Onglet: {selected_sheet}")
         df = st.session_state.data[selected_sheet]
-        
-        # Affichage du tableau
-        numeric_cols = df.select_dtypes(include=['float64', 'int64']).columns
-        formatter = {col: "{:.2f}" for col in numeric_cols}
-        st.dataframe(df.style.format(formatter))
+    
+      # Affichage du tableau
+      numeric_cols = df.select_dtypes(include=['float64', 'int64']).columns
+      formatter = {col: "{:.2f}" for col in numeric_cols}
+      st.dataframe(df.style.format(formatter))
         
         # Visualisations spécifiques selon l'onglet
         if selected_sheet == "Budget VS Actual":
